7b0397305ffaf15203d1f5a43bab5f25112949ee
AleksandrVititnev <sasha-aka2014@hotmail.com>
2021-11-08 15:21:35 +0300

    new double massive

--- hw/fourth_lab/first.cpp
+++ hw/fourth_lab/first.cpp
@@ -2,6 +2,15 @@
 #include "MyLib.h"
 
 int main() {
+    int x = 10;
+    double** table = new double* [x];
+    for (int x = 0; x < x; x++) {
+        table[x] = new double [x];
+    }
 
+    for (int x = 0; x < x; x++) {
+        delete[] *(table + x);
+    }
+    delete[] table;
     return 0;
 }
